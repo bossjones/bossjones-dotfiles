0ddbcabd61dbee2c9230b997792d573d0a04bf8f
Malcolm Jones <malcolm@adobe.com>
2017-03-14 17:30:10 -0400

    Chg: web_search plugin shellfn updates and bash_it

---
+++ bash_it/plugins/web_search.plugin.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+cite about-plugin
+about-plugin 'web_search from terminal'
+
+RED=`tput setaf 1`
+YELLOW=`tput setaf 3`
+
+web_search(){
+    declare -A urls
+    urls=(
+        [google]="https://www.google.com/search?q="
+        [bing]="https://www.bing.com/search?q="
+        [yahoo]="https://search.yahoo.com/search?p="
+        [duckduckgo]="https://www.duckduckgo.com/?q="
+        [yandex]="https://yandex.ru/yandsearch?text="
+        [github]="https://github.com/search?q="
+        [baidu]="https://www.baidu.com/s?wd="
+        [urban]="http://www.urbandictionary.com/define.php?term="
+        [regex]="https://regex101.com/library?orderBy=RELEVANCE&search="
+        [shell]="http://explainshell.com/explain?cmd="
+        [jsonb]="http://codebeautify.org/jsonviewer"
+        [aur]="https://www.archlinux.org/packages/?q="
+    )
+    args=( ${@} )
+
+    if [ $# -eq 0 ] || [ -z ${urls[$1]} ];then
+        echo ${RED}"Not a valid search engine !!"
+        return 1
+    fi
+
+    if [ $# -gt 1 ];then
+        url="${urls[$1]}""${args[@]:1}"
+
+    else
+        echo ${YELLOW}"Search term missing!!"
+        return 1
+    fi
+    xdg-open "$url" > /dev/null 2>&1
+}
+
+alias google='web_search google'
+alias bing='web_search bing'
+alias duckduckgo='web_search duckduckgo'
+alias yahoo='web_search yahoo'
+alias yandex='web_search yandex'
+alias github='web_search github'
+alias baidu='web_search baidu'
+
+# source: https://hackernoon.com/cross-platform-productivity-tool-with-terminal-7dd0487ead93#.6oxf3gey7
+alias urban='web_search urban'
+alias regex='web_search regex'
+alias shell='web_search shell'
+alias jsonb='web_search jsonb'
+alias aur='web_search aur'
+
+#duckduckgo !hacks
+alias wiki='web_search duckduckgo \!w'
+alias news='web_search duckduckgo \!n'
+alias youtube='web_search duckduckgo \!yt'
+alias map='web_search duckduckgo \!m'
+alias image='web_search duckduckgo \!i'
+alias ducky='web_search duckduckgo \!'
+alias twitter='web_search duckduckgo \!twitter'
+alias amazon='web_search duckduckgo \!a'
+alias torrent='web_search duckduckgo \!tpb'
