3de9fb9296a1e81c419e920ccd5901a2650837c3
Malcolm Jones <malcolm@adobe.com>
2017-03-14 14:01:52 -0400

    Chg: osx-shortcuts

---
+++ bin/osx-shortcuts.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+alias mac_model='sysctl hw.model'
+
+# volume-level (osx only)
+alias stfu="osascript -e 'set volume output muted true'"
+alias pumpitup="osascript -e 'set volume 10'"
+alias mid="osascript -e 'set volume output volume 45'"
